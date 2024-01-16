f3db533b56c7e71463072b897275e8fc50db182c
luke0421 <guddnr0421@naver.com>
2024-01-16 15:58:01 +0900

    add: BACK-END package structure first commit

---
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/controller/MemberController.java
@@ -0,0 +1,7 @@
+package jpabasic.project_7lans.controller;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberController {
+}
---
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/entity/Member.java
@@ -0,0 +1,11 @@
+package jpabasic.project_7lans.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+}
