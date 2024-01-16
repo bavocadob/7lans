25cd34356986dff2104bef55129ae7add04434e6
luke0421 <guddnr0421@naver.com>
2024-01-16 14:46:08 +0900

    BACK-END first commit

---
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/Project7lansApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.project_7lans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project7lansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project7lansApplication.class, args);
+	}
+
+}
---
+++ BACK/project_7lans/src/test/java/jpabasic/project_7lans/Project7lansApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.project_7lans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project7lansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
