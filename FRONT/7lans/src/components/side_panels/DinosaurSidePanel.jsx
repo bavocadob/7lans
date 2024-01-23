3562c53c5ac6c138af24b90d7c74097346687735
박주헌 <741u741@naver.com>
2024-01-23 17:44:14 +0900

    dinosaur-page side-panel on doing

--- FRONT/7lans/src/components/side_panels/DinosaurSidePanel.jsx
+++ FRONT/7lans/src/components/side_panels/DinosaurSidePanel.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const DinosaurSidePanel = () => {
+
+  const [sidePanelStatus, setSidePanelStatus] = useState(true)
+
+  const renderSidePanel = () => {
+    if (sidePanelStatus) {
+      return (
+        <div style={{
+          backgroundColor: 'rgb(255, 248, 223)',
+          padding: '2rem',
+          minHeight: '100vh',
+          height: '100%',
+          color: 'white',
+          minWidth: '300px'
+        }}>
+          <button onClick={() => setSidePanelStatus(false)}>-</button>
+        </div>
+      )
+    }
+    else {
+      return (
+        <button onClick={() => setSidePanelStatus(true)}>+</button>
+      )
+    }
+  }
+
   return (
-    <div>DinosaurSidePanel</div>
+    renderSidePanel()
   )
 }
 
--- FRONT/7lans/src/pages/DinosaurDictPage.jsx
+++ FRONT/7lans/src/pages/DinosaurDictPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import DinosaurSidePanel from '../components/side_panels/DinosaurSidePanel'
 
 const DinosaurDictPage = () => {
   return (
-    <div>DinosaurDictPage</div>
+    <div><DinosaurSidePanel/></div>
   )
 }
 
