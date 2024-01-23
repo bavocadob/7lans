51990082b28deadeb145588dd8f9ab843883b0ba
oksehun <sehun9803@naver.com>
2024-01-23 11:05:17 +0900

    filename change to Pascal

---
+++ FRONT/7lans/src/pages/admin_pages/AdminMainPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AdminMainPage = () => {
+  return <div>AdminMainPage</div>;
+};
--- FRONT/7lans/src/pages/child_pages/child_main_page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const child_main_page = () => {
-  return (
-    <div>child_main_page</div>
-  )
-}
-
-export default child_main_page
