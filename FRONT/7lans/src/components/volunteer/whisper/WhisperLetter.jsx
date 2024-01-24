5e5c11590b8181c3cb8848939f4237b234c19334
oksehun <sehun9803@naver.com>
2024-01-24 17:46:07 +0900

    bug: not working this file at click calender

--- FRONT/7lans/src/components/volunteer/whisper/WhisperLetter.jsx
+++ FRONT/7lans/src/components/volunteer/whisper/WhisperLetter.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+const Letter = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  &::placeholder {
+    font-size: 16px;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
 
 const WhisperLetter = () => {
-  return (
-    <div>WhisperLetter</div>
-  )
-}
+  const onChange = () => {};
+  const onSubmit = () => {};
 
-export default WhisperLetter
+  return (
+    <Letter onSubmit={onSubmit}>
+      <TextArea
+        required
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        placeholder="What is happening?!"
+      />
+    </Letter>
+  );
+};
+
+export default WhisperLetter;
