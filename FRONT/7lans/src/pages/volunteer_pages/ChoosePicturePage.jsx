6fd528d13fb3869a761597f041bc26c01b1d3c5f
Jo-hye-won <a01051910573@gmail.com>
2024-01-30 10:45:55 +0900

    update: before merge

--- FRONT/7lans/src/pages/volunteer_pages/ChoosePicturePage.jsx
+++ FRONT/7lans/src/pages/volunteer_pages/ChoosePicturePage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import NormalNav from '../../components/navs/NormalNav'
 import CommonSidePanel from '../../components/side_panels/CommonSidePanel'
+import Picture from '../../components/volunteer/calendar/Picture'
 
 const ChoosePicturePage = () => {
   const navigate = useNavigate(); // useNavigate 함수를 호출하여 navigate 함수를 가져옴
@@ -9,17 +10,37 @@ const ChoosePicturePage = () => {
 
   return (
     <>
-    <div>
-    <NormalNav />
-    </div>
-    <div className='container'>
-    <div className='-3'>
-    <CommonSidePanel />
-      <div className='row-9'> 화상 때 캡쳐한 사진들 & 동영상</div>
-    </div>
-    </div>
+      <div>
+      <NormalNav />
+      </div>
+
+      <div style={{display: 'flex'}}>
+        <div style={{width: '300px'}}>
+          <CommonSidePanel />
+        </div>
+        <div style={{width: '100%'}}> 
+          <Picture />
+        </div>
+      </div>
     </>
+    
   );
 };
 
 export default ChoosePicturePage; // 파일 이름을 PascalCase로 수정
+
+<>
+<div style={{
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100vh',
+  width: '100vw',
+}}>
+  <NormalNav />
+  <div style={{flex: 1, padding: '30px', backgroundColor: 'rgb(255, 226, 123)'}}>
+    <div style={{height: '100%',borderRadius: '20px', backgroundColor: 'rgb(255, 255, 255)'}}>
+      <Picture />
+    </div>
+  </div>
+</div>
+</>
