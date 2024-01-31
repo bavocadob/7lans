b7f4c57be95eabf21795ff60fb6880a1c18d2f45
jeoneunhui <jeon78eun@naver.com>
2024-01-31 16:45:27 +0900

    comment: add log comment in ActivityLogController

--- BACK/project_7lans/src/main/java/jpabasic/project_7lans/controller/ActivityLogController.java
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/controller/ActivityLogController.java
@@ -7,6 +7,7 @@ import jpabasic.project_7lans.dto.activityLog.ActivityLogRequestDto;
 import jpabasic.project_7lans.dto.activityLog.ActivityLogResponseDto;
 import jpabasic.project_7lans.service.ActivityLogServiceImpl;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/activityLog")
 @RequiredArgsConstructor
+@Slf4j
 public class ActivityLogController {
 
     private final ActivityLogServiceImpl activityLogServiceImpl;
@@ -28,9 +30,11 @@ public class ActivityLogController {
     // Res: activityLog id, 날짜 정보(년, 월, 일), 활동 일지 승인 여부
     @PostMapping(value = "/volunteer/list")
     public ResponseEntity<List<ActivityLogResponseDto.detailListByVolunteer>> detailListByVolunteer(@RequestBody @Valid ActivityLogRequestDto.detailListByVolunteer listReqDto) {
+        log.info("[ActivityLogController.detailListByVolunteer] data input from FRONT relationId: {} dateInfo: {}", listReqDto.getRelationId(), listReqDto.getDateInfo());
         try{
             System.out.println("ActivityController : " + listReqDto.getRelationId());
             List<ActivityLogResponseDto.detailListByVolunteer> dtoResList = activityLogServiceImpl.detailListByVolunteer(listReqDto);
+            log.info("[ActivityLogController.detailListByVolunteer] data output to FRONT size: {}", dtoResList.size());
             return new ResponseEntity<>(dtoResList, HttpStatus.OK);
         }catch(Exception e){
             e.printStackTrace();
@@ -44,8 +48,10 @@ public class ActivityLogController {
     // Res: activityLog id, 활동 일지 날짜(년, 월, 일), 활동 시간, 활동 기관, 봉사자 명, 활동 내용, 작성 완료 여부, 승인 여부
     @PostMapping(value = "/volunteer")
     public ResponseEntity<ActivityLogResponseDto.detailByVolunteer> detailByVolunteer(@RequestBody ActivityLogRequestDto.detailByVolunteer detailReqDto) {
+        log.info("[ActivityLogController.detailByVolunteer] data input from FRONT relationId: {} activityLogId: {}", detailReqDto.getRelationId(), detailReqDto.getActivityLogId());
         try{
             ActivityLogResponseDto.detailByVolunteer detailResDto = activityLogServiceImpl.detailByVolunteer(detailReqDto);
+            log.info("[ActivityLogController.detailByVolunteer] data output to FRONT");
             return new ResponseEntity<>(detailResDto, HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -59,8 +65,10 @@ public class ActivityLogController {
     // Res: 없음
     @PutMapping(value = "/volunteer/modify")
     public ResponseEntity modifyActivityLogByVolunteer(@RequestBody ActivityLogRequestDto.modifyByVolunteer modifyReqDto){
+        log.info("[ActivityLogController.modifyActivityLogByVolunteer] data input from FRONT activityLog: {}", modifyReqDto.getActivityLogId());
         try{
             activityLogServiceImpl.modifyActivityLogByVolunteer(modifyReqDto);
+            log.info("[ActivityLogController.modifyActivityLogByVolunteer] modify is done");
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -73,8 +81,10 @@ public class ActivityLogController {
     // Res: 없음
     @PutMapping(value = "/volunteer/writeDone")
     public ResponseEntity writeDoneActivityLogByVolunteer(@RequestBody ActivityLogRequestDto.writeDoneByVolunteer writeDoneReqDto){
+        log.info("[ActivityLogController.writeDoneActivityLogByVolunteer] activityLog: {}", writeDoneReqDto.getActivityLogId());
         try{
             activityLogServiceImpl.writeDoneActivityLogByVolunteer(writeDoneReqDto);
+            log.info("[ActivityLogController.writeDoneActivityLogByVolunteer] modify is done");
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -90,11 +100,13 @@ public class ActivityLogController {
     // Res: activityLog id, 제목, 봉사자 명, 아동 명, 날짜(년, 월, 일)
     @GetMapping(value = "/manager/disapprovedList/{centerId}")
     public ResponseEntity<List<ActivityLogResponseDto.listDisapprovedByManager>> listDisapprovedByManager (@PathVariable Long centerId) {
+        log.info("[ActivityLogController.listDisapprovedByManager] data input centerId: {}", centerId);
         ActivityLogRequestDto.listDisapprovedByManager reqDto = ActivityLogRequestDto.listDisapprovedByManager.builder()
                 .centerId(centerId)
                 .build();
         try{
             List<ActivityLogResponseDto.listDisapprovedByManager> resList = activityLogServiceImpl.listDisapprovedByManager(reqDto);
+            log.info("[ActivityLogController.listDisapprovedByManager] list is done size: {}", resList.size());
             return new ResponseEntity<>(resList, HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -107,12 +119,14 @@ public class ActivityLogController {
     // Res: activityLog id, 제목, 봉사자 명, 아동 명, 날짜(년, 월, 일)
     @GetMapping(value = "/manager/approvedList/{centerId}")
     public ResponseEntity<List<ActivityLogResponseDto.listApprovedByManager>> listApprovedByManager (@PathVariable Long centerId) {
+        log.info("[ActivityLogController.listApprovedByManager] centerId: {}", centerId);
         ActivityLogRequestDto.listApprovedByManager reqDto = ActivityLogRequestDto.listApprovedByManager.builder()
                 .centerId(centerId)
                 .build();
 
         try {
             List<ActivityLogResponseDto.listApprovedByManager> resList = activityLogServiceImpl.listApprovedByManager(reqDto);
+            log.info("[ActivityLogController.listApprovedByManager] list size: {}", resList.size());
             return new ResponseEntity<List<ActivityLogResponseDto.listApprovedByManager>>(resList, HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -125,8 +139,10 @@ public class ActivityLogController {
     // Res: activityLog id, 활동 일지 날짜(년, 월, 일), 활동 시간, 활동 기관, 봉사자 명, 활동 내용, 작성 완료 여부, 승인 여부
     @PostMapping(value = "/manager/detail")
     public ResponseEntity<ActivityLogResponseDto.detailByManager> detailByManager (@RequestBody ActivityLogRequestDto.detailByManager reqDto) {
+        log.info("[ActivityLogController.detailByManager] output activityLogId: {}", reqDto.getActivityLogId());
         try {
             ActivityLogResponseDto.detailByManager resDto = activityLogServiceImpl.detailByManager(reqDto);
+            log.info("[ActivityLogController.detailByManager] output activityLogId: {}", resDto.getActivityLogId());
             return new ResponseEntity<>(resDto, HttpStatus.OK);
         }catch (Exception e){
             e.printStackTrace();
@@ -137,8 +153,10 @@ public class ActivityLogController {
     //승인
     @PostMapping(value = "/manager/approve")
     public ResponseEntity approveByManager (@RequestBody ActivityLogRequestDto.approveByManager approveReqDto) {
+        log.info("[ActivityLogController.approveByManager] input activityLogId: {}", approveReqDto.getActivityLogId());
         try{
             activityLogServiceImpl.approveByManager(approveReqDto);
+            log.info("[ActivityLogController.approveByManager] approve is done");
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -148,8 +166,10 @@ public class ActivityLogController {
     //시작 시간 입력
     @PostMapping(value = "/manager/starttime")
     public ResponseEntity setStartTime (@RequestBody ActivityLogRequestDto.startTime startTime) {
+        log.info("[ActivityLogController.setStartTime] relationId: {}, time: {}", startTime.getMeetingId(), startTime.getStartTime());
         try{
             activityLogServiceImpl.setStartTime(startTime);
+            log.info("[ActivityLogController.setStartTime] set start time done");
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
@@ -159,8 +179,10 @@ public class ActivityLogController {
     //종료 시간 입력
     @PostMapping(value = "/manager/endtime")
     public ResponseEntity setEndTime (@RequestBody ActivityLogRequestDto.endTime endTime) {
+        log.info("[ActivityLogController.setEndTime] relationId: {}, time: {}", endTime.getMeetingId(), endTime.getEndTime());
         try{
             activityLogServiceImpl.setEndTime(endTime);
+            log.info("[ActivityLogController.setEndTime] set end time done");
             return new ResponseEntity(HttpStatus.OK);
         }catch (Exception e){
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
