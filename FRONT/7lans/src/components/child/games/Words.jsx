6a4e529491adc68e2be829629eb8ee45030d1e7e
Jo-hye-won <a01051910573@gmail.com>
2024-02-02 10:21:40 +0900

    style: finish game_style

--- FRONT/7lans/src/components/child/games/Words.jsx
+++ FRONT/7lans/src/components/child/games/Words.jsx
@@ -108,23 +108,23 @@ const Words = () => {
                     alignItems: 'center' }}>
         <h1 style={{ marginTop: '4%', 
                     fontWeight: 'bolder', 
-                    color: 'rgb(255, 215, 3)', 
-                    textShadow: '2px 2px 2px rgb(45, 45, 45)' }}>
+                    color: 'rgb(45, 45, 45)', 
+                    textShadow: '2px 2px 2px rgb(255, 215, 3)' }}>
           "{word}"을 포함한 문장을 만들어 보아요
         </h1>
         <div
           className='shadow'
           style={{display: 'flex',
                   flexDirection: 'column',
-                  border: '5px solid rgb(45, 45, 45)',
+                  border: '4px solid rgb(45, 45, 45)',
                   borderRadius: '20px',
                   width: '90%',
                   flex: 1,
                   margin: '2rem',
-                  backgroundColor: 'rgb(255, 250, 233)',
+                  // backgroundColor: 'rgb(255, 250, 233)',
           }}
         >
-          <div
+          <div className='shadow'
             style={{ display: 'flex',
                     flexWrap: 'wrap',
                     justifyContent: 'center',
@@ -132,18 +132,68 @@ const Words = () => {
                     textAlign: 'center',
                     backgroundColor: 'rgb(251, 243, 212)',
                     margin: '3%',
-                    borderRadius: '10px',
-                    border: '5px solid rgb(45, 45, 45)',
+                    marginTop: '40px',
+                    borderRadius: '15px',
+                    // border: '4px solid rgb(45, 45, 45)',
             }}
           >
-            <div style={{width: '100%', height: '100%', display: 'flex', flexDirection: 'column', padding: '1rem', justifyContent: 'center'}}>
-              <input style={{border: '5px solid rgb(45, 45, 45)', borderRadius: '10px', padding: '5px', margin: '0.5rem', fontSize: '20px'}} type="text" placeholder={word1? '': '문장을 완성해 주세요'} value={word1} onChange={(e) => setWord1(e.target.value)}/>
-              <input style={{border: '5px solid rgb(45, 45, 45)', borderRadius: '10px', padding: '5px', margin: '0.5rem', fontSize: '20px'}} type="text" placeholder={word2? '': '문장을 완성해 주세요'} value={word2} onChange={(e) => setWord2(e.target.value)}/>
-              <input style={{border: '5px solid rgb(45, 45, 45)', borderRadius: '10px', padding: '5px', margin: '0.5rem', fontSize: '20px'}} type="text" placeholder={word3? '': '문장을 완성해 주세요'} value={word3} onChange={(e) => setWord3(e.target.value)}/>
-              <input style={{border: '5px solid rgb(45, 45, 45)', borderRadius: '10px', padding: '5px', margin: '0.5rem', fontSize: '20px'}} type="text" placeholder={word4? '': '문장을 완성해 주세요'} value={word4} onChange={(e) => setWord4(e.target.value)}/>
+            <div style={{width: '100%', 
+            height: '100%', 
+            display: 'flex', 
+            flexDirection: 'column',
+            padding: '1rem', 
+            justifyContent: 'center'}}>
+              <input style={{border: '3px solid rgb(45, 45, 45)', 
+                              borderRadius: '10px', 
+                              padding: '5px', 
+                              margin: '0.5rem', 
+                              fontSize: '20px'}} 
+                              type="text" 
+                      placeholder={word1? '': '문장을 완성해 주세요'} 
+                      value={word1} 
+                      onChange={(e) => setWord1(e.target.value)}/>
+              <input style={{border: '3px solid rgb(45, 45, 45)', 
+                              borderRadius: '10px', 
+                              padding: '5px', 
+                              margin: '0.5rem', 
+                              fontSize: '20px'}} 
+                              type="text" 
+                      placeholder={word2? '': '문장을 완성해 주세요'} 
+                      value={word2} 
+                      onChange={(e) => setWord2(e.target.value)}/>
+              <input style={{border: '3px solid rgb(45, 45, 45)', 
+                              borderRadius: '10px', 
+                              padding: '5px', 
+                              margin: '0.5rem', 
+                              fontSize: '20px'}} 
+                              type="text" 
+                      placeholder={word3? '': '문장을 완성해 주세요'}  
+                      value={word3} 
+                      onChange={(e) => setWord3(e.target.value)}/>
+              <input style={{border: '3px solid rgb(45, 45, 45)', 
+                              borderRadius: '10px', 
+                              padding: '5px', 
+                              margin: '0.5rem', 
+                              fontSize: '20px'}} 
+                              type="text" 
+                      placeholder={word4? '': '문장을 완성해 주세요'} 
+                      value={word4} 
+                      onChange={(e) => setWord4(e.target.value)}/>
             </div>
           </div>
-          <button className='shadow' style={{width: '150px', alignSelf: 'center', fontWeight: 'bolder', fontSize: '20px', border: 'none', borderRadius: '20px', backgroundColor: 'rgb(255, 215, 3)'}} onClick={reset}>제출</button>
+          <button className='shadow' 
+                  style={{width: '150px', 
+                          height:'60px',
+                          alignSelf: 'center', 
+                          fontWeight: 'bolder', 
+                          fontSize: '25px', 
+                          border: 'none', 
+                          borderRadius: '16px', 
+                          backgroundColor: 'rgb(255, 215, 3)',
+                          marginBottom: '20px'
+                        }} 
+                  onClick={reset}>제출
+          </button>
         </div>
       </div>
     )
