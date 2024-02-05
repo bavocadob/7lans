f2ce56915c1c4af93f140eb834c5b6d71c35054e
강성은 <1ksy3629@naver.com>
2024-02-05 12:13:12 +0900

    update: Move shuffle method above its invocation

--- FRONT/7lans/src/components/volunteer/games/CardFind.jsx
+++ FRONT/7lans/src/components/volunteer/games/CardFind.jsx
@@ -2,13 +2,17 @@ import React, { useEffect, useState } from 'react'
 
 const cardNum = Array.from({ length: 16 }, (_, index) => index + 1);
 const CardFind = () => {
-    
+
   const [flippedCard, setFlippedCard] = useState([])
   const [nowCard, setNowCard] = useState([])
   const [mount, setMount] = useState([])
   const [flipping, setFlipping] = useState(false)
   const [correct, setCorrect] = useState(false)
-  
+
+  const shuffle = (arr) => {
+      arr.sort(() => Math.random() -0.5)
+  }
+
 
   useEffect(() => {
     shuffle(cardNum)
@@ -25,7 +29,7 @@ const CardFind = () => {
                   setNowCard([])
                   setFlipping(false)
               }, 1000)
-              
+
               return () => clearTimeout(timeoutfunc)
           }
           else {
@@ -34,7 +38,7 @@ const CardFind = () => {
                   setNowCard([])
                   setFlipping(false)
               }, 1000)
-              
+
               return () => clearTimeout(timeoutfunc)
           }
       }
@@ -56,10 +60,6 @@ const CardFind = () => {
     setNowCard((nowCard) => [...nowCard, num])
   }
 
-  const shuffle = (arr) => {
-    arr.sort(() => Math.random() -0.5)
-  }
-  
   const renderCard = () => {
     if (correct) {
         return (
@@ -75,38 +75,38 @@ const CardFind = () => {
     }
     else {
         return (
-            <div style={{display: 'flex', 
-                        flexWrap: 'wrap', 
-                        height: '100%', 
-                        width: '100%', 
-                        padding: '0' , 
-                        alignItems: 'center', 
+            <div style={{display: 'flex',
+                        flexWrap: 'wrap',
+                        height: '100%',
+                        width: '100%',
+                        padding: '0' ,
+                        alignItems: 'center',
                         justifyContent: 'center'}}>
                 {cardNum.map((num, index) => {
                     const isFlipped = nowCard.includes(num) || flippedCard.includes(num)
                     return (
-                    <button disabled={(flippedCard.includes(num) || flipping)? true : false} 
-                            style={{transform: (isFlipped ? 'rotateY(360deg)' : ''), 
-                                    transition: 'transform 0.4s', 
-                                    fontSize: '2rem' , 
-                                    height: '120px', 
-                                    width: '140px', 
+                    <button disabled={(flippedCard.includes(num) || flipping)? true : false}
+                            style={{transform: (isFlipped ? 'rotateY(360deg)' : ''),
+                                    transition: 'transform 0.4s',
+                                    fontSize: '2rem' ,
+                                    height: '120px',
+                                    width: '140px',
                                     margin: '1rem',
-                                    marginBottom: '0', 
-                                    border: '3px solid rgb(240, 165, 8)', 
-                                    borderRadius: '17px', 
+                                    marginBottom: '0',
+                                    border: '3px solid rgb(240, 165, 8)',
+                                    borderRadius: '17px',
                                     backgroundColor: 'rgb(255, 215, 3)',
-                                  }} 
-                            key={index} 
+                                  }}
+                            key={index}
                             onClick={(e) => handleCard(e, num)}>
-                              {(nowCard.includes(num) || flippedCard.includes(num))? 
-                              (num > 8 ? <img src={`./cardImage/cardImage${num-8}.png`} 
-                                              alt="" 
-                                              style={{height: '60px', 
-                                                      width: '60px'}} /> : 
-                              <img src={`./cardImage/cardImage${num}.png`} 
-                                   alt="" 
-                                   style={{height: '60px', 
+                              {(nowCard.includes(num) || flippedCard.includes(num))?
+                              (num > 8 ? <img src={`./cardImage/cardImage${num-8}.png`}
+                                              alt=""
+                                              style={{height: '60px',
+                                                      width: '60px'}} /> :
+                              <img src={`./cardImage/cardImage${num}.png`}
+                                   alt=""
+                                   style={{height: '60px',
                                            width: '60px'}} />) : ''}
                             <img src={`./cardImage/cardImage${num}`} alt="" />
                     </button>
