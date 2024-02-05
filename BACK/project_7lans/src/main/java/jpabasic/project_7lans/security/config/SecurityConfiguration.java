967dc54c4890a0e79d912b30c0d810070ab74c74
guddnr0421 <guddnr0421@naver.com>
2024-02-05 14:45:10 +0900

    update: SecurityConfiguration cors

--- BACK/project_7lans/src/main/java/jpabasic/project_7lans/security/config/SecurityConfiguration.java
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/security/config/SecurityConfiguration.java
@@ -47,6 +47,7 @@ public class SecurityConfiguration {
                             // hasRole는 SpringSecurity 가 자동으로 가장 앞에 Role_를 붙인다. 따라서 hasAuthority를 쓰는 것이 낫다.
                             // authorizeRequests.requestMatchers("/vol/**").hasRole(MemberType.VOLUNTEER.name());
                             // authorizeRequests.requestMatchers("/vol/**").hasAuthority(MemberType.VOLUNTEER.name());
+                            authorizeRequests.requestMatchers("/**").permitAll();
                             authorizeRequests.anyRequest().permitAll();
                         }
                 );
