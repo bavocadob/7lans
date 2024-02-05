acbec70d3d864c8331f767d5d688e01066f23596
guddnr0421 <guddnr0421@naver.com>
2024-02-05 15:41:19 +0900

    update: corsFilter allow all

--- BACK/project_7lans/src/main/java/jpabasic/project_7lans/security/config/CorsConfig.java
+++ BACK/project_7lans/src/main/java/jpabasic/project_7lans/security/config/CorsConfig.java
@@ -6,6 +6,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -15,13 +16,17 @@ public class CorsConfig {
     public CorsFilter corsFilter(){
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
         config.setAllowCredentials(false);
-        config.setAllowedOrigins(List.of("*"));
-        config.addAllowedOrigin("https://localhost:30007");
-        config.addAllowedOrigin("https://localhost:30007");
-        config.addAllowedOrigin("https://i10e103.p.ssafy.io");
-//        config.addAllowedHeader("*");
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+//        config.setAllowedOriginPatterns(Arrays.asList("*"));
+//        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowCredentials(true);
+
+        //config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
