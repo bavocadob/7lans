0649a223af7b6c697df1d38a8bfb0f7fc50296d2
oksehun <sehun9803@naver.com>
2024-02-06 17:02:31 +0900

    update:url change

--- FRONT/7lans/src/store/urlSlice.jsx
+++ FRONT/7lans/src/store/urlSlice.jsx
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    //value: 'http://localhost:8080'
-    value: 'https://i10e103.p.ssafy.io/api/v1'
-    
-}
+  //value: 'http://localhost:8080'
+  // value: 'https://i10e103.p.ssafy.io/api/v1'
+  value: "http://mydevlogme.iptime.org:8080",
+};
 
 const urlSlice = createSlice({
   name: "url",
