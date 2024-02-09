f40f1d96256f0f2972e9c51c13b6cd44b1f06fa2
조혜원 <a01051910573@gmail.com>
2024-02-09 12:40:25 +0900

    feat: tooltip to recordbutton

--- FRONT/7lans/src/components/Audio.jsx
+++ FRONT/7lans/src/components/Audio.jsx
@@ -1,35 +1,36 @@
 import React, { useState, useCallback } from "react";
 import { Howl, Howler } from "howler";
 import styled from "styled-components";
+import { Tooltip } from "react-tooltip";
 
 const Buttondiv = styled.div`
   display: flex;
   flex-direction: row;
   gap: 40px;
-  margin-left: 40px;
-`
+  margin-left: 60px;
+`;
 
 const Button = styled.button`
   background: linear-gradient(
-              300deg,
-              rgba(255, 184, 36, 1),
-              rgba(255, 237, 140, 1));
-  border: 3px solid rgba(45,45,45);
+    300deg,
+    rgba(255, 184, 36, 1),
+    rgba(255, 237, 140, 1)
+  );
+  border: 3px solid rgba(45, 45, 45);
   border-radius: 100px;
   width: 160px;
   height: 110px;
   font-size: 27px;
-`
+`;
 
-
-const ChildAudioPlay = ({dinoState, setdinoState}) => {
+const ChildAudioPlay = ({ dinoState, setdinoState }) => {
   const [stream, setStream] = useState();
   const [media, setMedia] = useState();
   const [onRec, setOnRec] = useState(true);
   const [source, setSource] = useState();
   const [analyser, setAnalyser] = useState();
   const [audioUrl, setAudioUrl] = useState();
-  const [disabled, setDisabled] = useState(true); 
+  const [disabled, setDisabled] = useState(true);
   const [isRecording, setIsRecording] = useState(false);
 
   const toggleRecording = () => {
@@ -45,8 +46,8 @@ const ChildAudioPlay = ({dinoState, setdinoState}) => {
   };
 
   const onRecAudio = () => {
-    setDisabled(true) 
-    
+    setDisabled(true);
+
     // 음원정보를 담은 노드를 생성하거나 음원을 실행또는 디코딩 시키는 일을 한다
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
     // 자바스크립트를 통해 음원의 진행상태에 직접접근에 사용된다.
@@ -109,32 +110,33 @@ const ChildAudioPlay = ({dinoState, setdinoState}) => {
     // 메서드가 호출 된 노드 연결 해제
     analyser.disconnect();
     source.disconnect();
-    
+
     if (audioUrl) {
       URL.createObjectURL(audioUrl); // 출력된 링크에서 녹음된 오디오 확인 가능
     }
-    
+
     // File 생성자를 사용해 파일로 변환
     const sound = new File([audioUrl], "soundBlob", {
       lastModified: new Date().getTime(),
       type: "audio",
     });
-  	
+
     setDisabled(false);
     console.log(sound); // File 정보 출력
   };
 
-  const play = () => { 
-    setdinoState(true)
-    const audio = new Audio(URL.createObjectURL(audioUrl))
+  const play = () => {
+    setdinoState(true);
+    const audio = new Audio(URL.createObjectURL(audioUrl));
     const sound = new Howl({
       src: [audio.src],
       format: ["mp3", "ogg"],
       volume: 1,
       rate: 1.5, // 속도를 조절하여 음성을 변조
-      onend: function() { // 재생이 끝나면 호출될 콜백 함수, 공룡 상태 변경
+      onend: function () {
+        // 재생이 끝나면 호출될 콜백 함수, 공룡 상태 변경
         setdinoState(false);
-      }
+      },
     });
     console.log(audio.src);
 
@@ -144,11 +146,37 @@ const ChildAudioPlay = ({dinoState, setdinoState}) => {
   return (
     <>
       <Buttondiv>
-        <Button onClick={toggleRecording}> {isRecording ? '녹음완료' : '녹음시작'} </Button>
-        <Button onClick={play} disabled={disabled}>재생</Button>
+        <a data-tooltip-id="record_tooltip">
+          <Button
+            onClick={toggleRecording}
+            // title={isRecording ? '' : '녹음을 하면 공룡이 따라 말해요'}
+          >
+            {isRecording ? "녹음완료" : "녹음시작"}
+          </Button>{" "}
+        </a>
+        <Tooltip id="record_tooltip">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>버튼을 누르면</span>
+            <span>녹음이 시작되요!</span>
+            <span>공룡에게 듣고싶은말이 있나요?</span>
+          </div>
+        </Tooltip>
+
+        <a data-tooltip-id="start-tooltip">
+          <Button onClick={play} disabled={disabled}>
+            재생
+          </Button>
+        </a>
+        <Tooltip id="start-tooltip">
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>버튼을 누르면</span>
+            <span>녹음한 대사 그대로</span>
+            <span>공룡이 말을 해요!!</span>
+          </div>
+        </Tooltip>
       </Buttondiv>
     </>
   );
-}
+};
 
 export default ChildAudioPlay;
--- FRONT/7lans/src/pages/volunteer_pages/RaiseEggPage.jsx
+++ FRONT/7lans/src/pages/volunteer_pages/RaiseEggPage.jsx
@@ -46,6 +46,7 @@ const RowBox3 = styled.div`
   font-size: 20px;
   display: flex;
   align-items: end;
+  padding-left: 55px;
 `;
 const ExpBar = styled.div`
   width: 100%;
