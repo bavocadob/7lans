ff87bad1f0863121a6d5e1fd2a69e05b2f5eef74
박주헌 <741u741@naver.com>
2024-02-11 14:54:47 +0900

    fix: child-page-sidepanel size fix

--- FRONT/7lans/src/pages/child_pages/ChildVideoChattingStartPage.jsx
+++ FRONT/7lans/src/pages/child_pages/ChildVideoChattingStartPage.jsx
@@ -1,21 +1,23 @@
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import CommonSidePanel from '../../components/side_panels/CommonSidePanel';
 import NormalNav from '../../components/navs/NormalNav';
-import PostIt from '../../components/volunteer/post_it/PostIt';
-import SelectedPostit from '../../components/volunteer/post_it/SelectedPostit';
-import VolunteerCalendar from '../../components/volunteer/calendar/VolunteerCalendar';
-import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
-import { updateChildInfo } from '../../store/childSlice';
-import { updateUserInfo } from '../../store/userSlice';
-import {updateChildrenInfo} from '../../store/childrenSlice';
 import Wrong from '../../components/dinosaur/Wrong';
 import SelectedChildPostit from '../../components/volunteer/post_it/SelectedChildPostit';
 import ChildPostit from '../../components/volunteer/post_it/ChildPostit';
 import ChildCommonSidePanel from '../../components/side_panels/ChildCommonSidePanel';
 import ChildCalendar from '../../components/volunteer/calendar/ChildCalendar';
+
+const RightSide = styled.div`
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-content: center;
+  border-radius: 0 20px 20px 0;
+  background-color: rgb(255,255,255);
+`
 
 const ChildVideoChattingStartPage = () => {
   const [data, setData] = useState(0);
@@ -27,21 +29,20 @@ const ChildVideoChattingStartPage = () => {
   
   return (
     <>
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      width: '100vw',
-    }}>
       <NormalNav />
-      <div style={{ marginTop: "5.7%" }}></div>
-      <div style={{flex: 1, padding: '30px', backgroundColor: 'rgb(255, 226, 123)'}}>
-        <div style={{height: '100%', width: '100%', display: 'flex', borderRadius: '20px', backgroundColor: 'rgb(255, 226, 123)'}}>
-          <ChildCommonSidePanel 
-            />
-            <div>
-            </div>
-          <div style={{width: '90%', flex: 1, borderRadius: '0 20px 20px 0', backgroundColor: 'rgb(255, 255, 255)'}}>
+      <div style={{ marginTop: "5.7%" }}>
+      <div style={{ height: '650px',
+                    padding: '30px', 
+                    paddingBottom: "20px",
+                    backgroundColor: 'rgb(255, 226, 123)'}}>
+        <div style={{height: '100%', 
+                      width: '100%', 
+                      display: 'flex', 
+                      flexDirection: 'row',
+                      borderRadius: '20px', 
+                      backgroundColor: 'rgb(255, 226, 123)'}}>
+          <ChildCommonSidePanel />
+          <RightSide>
             {volInfo? 
               <ChildCalendar 
                 child={data}
@@ -53,7 +54,7 @@ const ChildVideoChattingStartPage = () => {
               </div>
             }     
 
-          </div>
+          </RightSide>
 
           <div style={{width: '10%', backgroundColor: 'rgb(255, 226, 123)'}}>
             <SelectedChildPostit message={'/child_video_chatting_start'}/>
@@ -64,7 +65,7 @@ const ChildVideoChattingStartPage = () => {
           <div style={{display: 'flex', flexDirection: 'column', position: 'absolute', right: '2%', top: '10rem'}}>
           </div>
       </div>
-    </div>
+      </div>
     </>
   );
 };
